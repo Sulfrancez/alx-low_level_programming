e9ecdd85bda761ca3893ddd2874f6be5099927cf
Francis <defranson@gmail.com>
2022-09-14 21:53:23 -0700

    task 12 to 14 added

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ *
+ * @n: number of the times table
+ */
+
+void print_times_table(int n)
+{
+
+_putchar('\n');
+
+_putchar('\n');
+
+_putchar('\n');
+
+
+}

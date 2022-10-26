dfbbc4e157c08e6177c8872d4a8bf6043627397d
sulfrancez <sulfrancez@gmail.com>
2022-10-26 05:59:45 +0100

    all

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: double pointer to head of list
+ * @n: integer to be added
+ * Return: address of node added
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+  listint_t *newnode = malloc(sizeof(listint_t));
+  listint_t *current;
+
+  if (!head || !newnode)
+    return (NULL);
+  newnode->n = n;
+  newnode->next = NULL;
+  if (!*head)
+    *head = newnode;
+  else
+    {
+      current = *head;
+      while (current->next)
+	current = current->next;
+      current->next = newnode;
+    }
+  return (newnode);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: double pointer to head of list
+ * @idx: index to add new node
+ * @n: value at new node
+ * Return: address of new node, or NULL if failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+  listint_t *current;
+  listint_t *newnode;
+
+  register uint i;
+
+  newnode = malloc(sizeof(listint_t));
+  if (!head || !newnode)
+    return (NULL);
+  newnode->n = n;
+  if (!idx)
+    {
+      newnode->next = *head;
+      *head = newnode;
+      return (newnode);
+    }
+  current = *head;
+  while (current)
+    {
+      if (i == idx - 1)
+	{
+	  newnode->next = current->next;
+	  current->next = newnode;
+	  return (newnode);
+	}
+      i++;
+      current = current->next;
+    }
+  free(newnode);
+  return (NULL);
+}

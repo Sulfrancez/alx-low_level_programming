c7964634ddcd88708db2a80e4b18e1ea3a41e5c7
sulfrancez <sulfrancez@gmail.com>
2022-12-14 01:53:22 +0100

    task1

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * @n: largest number of bytes to append
+ * Return: address of dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+
+	i = j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
